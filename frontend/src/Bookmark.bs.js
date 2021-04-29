4b3de57626fd15d5c466325f85ae151715f6090a
Neil Parikh <parikh.neil@me.com>
2021-04-29 19:12:40 +0000

    use HeroIcons react library instead

--- frontend/src/Bookmark.bs.js
+++ frontend/src/Bookmark.bs.js
@@ -2,6 +2,8 @@
 
 import * as React from "react";
 import * as Belt_Array from "bs-platform/lib/es6/belt_Array.js";
+import TrashIcon from "@heroicons/react/solid/TrashIcon";
+import PencilIcon from "@heroicons/react/solid/PencilIcon";
 
 function Bookmark(Props) {
   var title = Props.title;
@@ -11,29 +13,11 @@ function Bookmark(Props) {
               className: "p-4 lg:width-1/2 flex flex-row space-x-4"
             }, React.createElement("div", {
                   className: "flex flex-col space-y-2"
-                }, React.createElement("svg", {
-                      className: "h-6 w-6",
-                      fill: "none",
-                      stroke: "currentColor",
-                      viewBox: "0 0 24 24",
-                      xmlns: "http://www.w3.org/2000/svg"
-                    }, React.createElement("path", {
-                          d: "M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16",
-                          strokeLinecap: "round",
-                          strokeLinejoin: "round",
-                          strokeWidth: "2"
-                        })), React.createElement("svg", {
-                      className: "h-6 w-6",
-                      fill: "none",
-                      stroke: "currentColor",
-                      viewBox: "0 0 24 24",
-                      xmlns: "http://www.w3.org/2000/svg"
-                    }, React.createElement("path", {
-                          d: "M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z",
-                          strokeLinecap: "round",
-                          strokeLinejoin: "round",
-                          strokeWidth: "2"
-                        }))), React.createElement("div", {
+                }, React.createElement(PencilIcon, {
+                      className: "h-6 w-6"
+                    }), React.createElement(TrashIcon, {
+                      className: "h-6 w-6"
+                    })), React.createElement("div", {
                   className: "flex flex-col"
                 }, React.createElement("a", {
                       className: "text-blue-500",
---
+++ frontend/src/bindings/Heroicons.res
@@ -0,0 +1,13 @@
+module Pencil = {
+  @bs.module("@heroicons/react/solid/PencilIcon") @react.component
+  external make: (
+    ~className: string
+  ) => React.element = "default"
+}
+
+module Trash = {
+  @bs.module("@heroicons/react/solid/TrashIcon") @react.component
+  external make: (
+    ~className: string
+  ) => React.element = "default"
+}
